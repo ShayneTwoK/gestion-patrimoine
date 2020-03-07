1a9f0825570531437bc3d1ed19923e2001123f8f
Shayne <khaldi.alternance@gmail.com>
2020-03-07 20:05:07 +0100

    feat-GP-0006 : manque class mainActivity pour vÃerif permission geolocalisation

--- app/src/main/java/sio/example/gestion_patrimoine/MapsActivity.java
+++ app/src/main/java/sio/example/gestion_patrimoine/MapsActivity.java
@@ -1,8 +1,15 @@
 package sio.example.gestion_patrimoine;
 
+import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.FragmentActivity;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -13,30 +20,69 @@ import com.google.android.gms.maps.model.MarkerOptions;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
+    // CONSTANTES
     private GoogleMap mMap;
+    private static final String FINE_LOCATION = Manifest.permission.ACCESS_FINE_LOCATION;
+    private static final String ACCESS_COARSE_LOCATION = Manifest.permission.ACCESS_COARSE_LOCATION;
+    private static final int LOCATION_PERMISSION_REQUEST_CODE = 1234;
+
+    // VARIABLES
+    private Boolean permissionAutorisee = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
-        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+
+        getPermissionDeLocalisation();
+
+    }
+
+    private void initGoogleMap() {
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
     }
 
+    private void getPermissionDeLocalisation() {
+        String[] permissions = {FINE_LOCATION, ACCESS_COARSE_LOCATION};
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
+        if (ContextCompat.checkSelfPermission(this.getApplicationContext(),
+                FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            if (ContextCompat.checkSelfPermission(this.getApplicationContext(),
+                    ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+                permissionAutorisee = true;
+            } else {
+                ActivityCompat.requestPermissions(this,
+                        permissions,
+                        LOCATION_PERMISSION_REQUEST_CODE);
+            }
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        permissionAutorisee = false;
+        switch (requestCode) {
+            case LOCATION_PERMISSION_REQUEST_CODE: {
+                if (grantResults.length > 0) {
+                    for (int result : grantResults) {
+                        if (result != PackageManager.PERMISSION_GRANTED) {
+                            permissionAutorisee = false;
+                            return;
+                        }
+                    }
+                    permissionAutorisee = true;
+                    // Initialiser la map Google ici
+                    initGoogleMap();
+                }
+            }
+        }
+    }
+
     @Override
     public void onMapReady(GoogleMap googleMap) {
+        Toast.makeText(this, "Bienvenue sur Cékoica la map des monuments !", Toast.LENGTH_SHORT).show();
         mMap = googleMap;
 
         // Add a marker in Sydney and move the camera
