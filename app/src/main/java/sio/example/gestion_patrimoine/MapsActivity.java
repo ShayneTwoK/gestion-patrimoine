f2b765c18989e653618ac9ac21a42915309dd297
Shayne <khaldi.alternance@gmail.com>
2020-03-08 17:44:02 +0100

    feat-GP-0006 : mise a jour mainActivity pour afficher permissions a l'arrive de maps

--- app/src/main/java/sio/example/gestion_patrimoine/MapsActivity.java
+++ app/src/main/java/sio/example/gestion_patrimoine/MapsActivity.java
@@ -1,93 +1,101 @@
 package sio.example.gestion_patrimoine;
 
-import androidx.annotation.NonNull;
-import androidx.core.app.ActivityCompat;
-import androidx.core.content.ContextCompat;
-import androidx.fragment.app.FragmentActivity;
-
 import android.Manifest;
-import android.content.Context;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Toast;
 
-import com.google.android.gms.maps.CameraUpdateFactory;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
-import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+public class MapsActivity extends AppCompatActivity implements OnMapReadyCallback {
 
     // CONSTANTES
-    private GoogleMap mMap;
+    private static final String TAG = "MapActivity";
     private static final String FINE_LOCATION = Manifest.permission.ACCESS_FINE_LOCATION;
-    private static final String ACCESS_COARSE_LOCATION = Manifest.permission.ACCESS_COARSE_LOCATION;
+    private static final String COARSE_LOCATION = Manifest.permission.ACCESS_COARSE_LOCATION;
     private static final int LOCATION_PERMISSION_REQUEST_CODE = 1234;
 
     // VARIABLES
-    private Boolean permissionAutorisee = false;
+    private Boolean permissionsAccordee = false;
+    private GoogleMap mMap;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onMapReady(GoogleMap googleMap) {
+        Toast.makeText(this, "Map Prête", Toast.LENGTH_SHORT).show();
+        Log.d(TAG, "onMapReady: map correctement afficher");
+        mMap = googleMap;
+    }
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
 
-        getPermissionDeLocalisation();
-
+        getPermissionLocalisation();
     }
 
-    private void initGoogleMap() {
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
-                .findFragmentById(R.id.map);
-        mapFragment.getMapAsync(this);
+    private void initMap() {
+        Log.d(TAG, "initMap: Map Initialisé");
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+
+        mapFragment.getMapAsync(MapsActivity.this);
     }
 
-    private void getPermissionDeLocalisation() {
-        String[] permissions = {FINE_LOCATION, ACCESS_COARSE_LOCATION};
+    private void getPermissionLocalisation() {
+        Log.d(TAG, "getLocationPermission: Permissions de localisation");
+        String[] permissions = {FINE_LOCATION, COARSE_LOCATION};
 
         if (ContextCompat.checkSelfPermission(this.getApplicationContext(),
                 FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
             if (ContextCompat.checkSelfPermission(this.getApplicationContext(),
-                    ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
-                permissionAutorisee = true;
+                    COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+                permissionsAccordee = true;
+                initMap();
             } else {
                 ActivityCompat.requestPermissions(this,
                         permissions,
                         LOCATION_PERMISSION_REQUEST_CODE);
             }
+        } else {
+            ActivityCompat.requestPermissions(this,
+                    permissions,
+                    LOCATION_PERMISSION_REQUEST_CODE);
         }
     }
 
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        permissionAutorisee = false;
+        Log.d(TAG, "onRequestPermissionsResult: Appel de la méthode");
+        permissionsAccordee = false;
+
         switch (requestCode) {
             case LOCATION_PERMISSION_REQUEST_CODE: {
                 if (grantResults.length > 0) {
-                    for (int result : grantResults) {
-                        if (result != PackageManager.PERMISSION_GRANTED) {
-                            permissionAutorisee = false;
+                    for (int grantResult : grantResults) {
+                        if (grantResult != PackageManager.PERMISSION_GRANTED) {
+                            permissionsAccordee = false;
+                            Log.d(TAG, "onRequestPermissionsResult: Permission refusé");
                             return;
                         }
                     }
-                    permissionAutorisee = true;
-                    // Initialiser la map Google ici
-                    initGoogleMap();
+                    Log.d(TAG, "onRequestPermissionsResult: Permission accordé");
+                    permissionsAccordee = true;
+                    // Initialisé la Map Google
+                    initMap();
                 }
             }
         }
     }
 
-    @Override
-    public void onMapReady(GoogleMap googleMap) {
-        Toast.makeText(this, "Bienvenue sur Cékoica la map des monuments !", Toast.LENGTH_SHORT).show();
-        mMap = googleMap;
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
-    }
 }
