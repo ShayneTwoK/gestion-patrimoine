be5ef53e8567de98393e9318c7e09e958adfa1d7
Shayne <khaldi.alternance@gmail.com>
2020-03-08 19:32:55 +0100

    feat/GP-0006_Ajout_page_accueil : erreur Java location.addOnCompleteListener Build Fail

---
+++ app/src/main/java/sio/example/gestion_patrimoine/LocalisationDevice.java
@@ -0,0 +1,12 @@
+package sio.example.gestion_patrimoine;
+
+import android.util.Log;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+
+public class LocalisationDevice{
+
+
+
+}
--- app/src/main/java/sio/example/gestion_patrimoine/MapsActivity.java
+++ app/src/main/java/sio/example/gestion_patrimoine/MapsActivity.java
@@ -3,6 +3,7 @@ package sio.example.gestion_patrimoine;
 import android.Manifest;
 import android.app.Dialog;
 import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.Toast;
@@ -15,28 +16,42 @@ import androidx.core.content.ContextCompat;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.GoogleApiAvailability;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 
 public class MapsActivity extends AppCompatActivity implements OnMapReadyCallback {
 
     // CONSTANTES
-    private static final String TAG = "MapActivity";
+    private static final String TAGMap = "MapActivity";
+    private static final String TAGLocalisation = "Localisation";
     private static final int ERROR_DIALOG_REQUEST = 9001;
     private static final String FINE_LOCATION = Manifest.permission.ACCESS_FINE_LOCATION;
     private static final String COARSE_LOCATION = Manifest.permission.ACCESS_COARSE_LOCATION;
     private static final int LOCATION_PERMISSION_REQUEST_CODE = 1234;
+    private static final float DEFAULT_ZOOM = 15f;
 
     // VARIABLES
     private Boolean permissionsAccordee = false;
     private GoogleMap mMap;
+    private FusedLocationProviderClient mFusedLocationProviderClient;
+    private MapsActivity MapsActivity;
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
         Toast.makeText(this, "Map Prête", Toast.LENGTH_SHORT).show();
-        Log.d(TAG, "onMapReady: map correctement afficher");
+        Log.d(TAGMap, "onMapReady: map correctement afficher");
         mMap = googleMap;
+
+        if (permissionsAccordee) {
+            getLocalisationDevice();
+        }
     }
 
     @Override
@@ -50,16 +65,16 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
         }
 
     }
-
+    ///// INITIALISE LA MAP /////
     private void initMap() {
-        Log.d(TAG, "initMap: Map Initialisé");
+        Log.d(TAGMap, "initMap: Map Initialisé");
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
 
         mapFragment.getMapAsync(MapsActivity.this);
     }
-
+    ///// RECUPERE LA PERMISSION DONNER PAR LE USER /////
     private void getPermissionLocalisation() {
-        Log.d(TAG, "getLocationPermission: Permissions de localisation");
+        Log.d(TAGMap, "getLocationPermission: Permissions de localisation");
         String[] permissions = {FINE_LOCATION, COARSE_LOCATION};
 
         if (ContextCompat.checkSelfPermission(this.getApplicationContext(),
@@ -79,10 +94,10 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
                     LOCATION_PERMISSION_REQUEST_CODE);
         }
     }
-
+    ///// RETOUR DE LA DEMANDE DE PERMISSION DU GPS /////
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        Log.d(TAG, "onRequestPermissionsResult: Appel de la méthode");
+        Log.d(TAGMap, "onRequestPermissionsResult: Appel de la méthode");
         permissionsAccordee = false;
 
         switch (requestCode) {
@@ -91,11 +106,11 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
                     for (int grantResult : grantResults) {
                         if (grantResult != PackageManager.PERMISSION_GRANTED) {
                             permissionsAccordee = false;
-                            Log.d(TAG, "onRequestPermissionsResult: Permission refusé");
+                            Log.d(TAGMap, "onRequestPermissionsResult: Permission refusé");
                             return;
                         }
                     }
-                    Log.d(TAG, "onRequestPermissionsResult: Permission accordé");
+                    Log.d(TAGMap, "onRequestPermissionsResult: Permission accordé");
                     permissionsAccordee = true;
                     // Initialisé la Map Google
                     initMap();
@@ -103,19 +118,19 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
             }
         }
     }
-
+    ///// VERIFICATION DES SERVICES GOOGLE FONCTIONELS /////
     public boolean isServicesOK() {
-        Log.d(TAG, "isServicesOK: checking google services version");
+        Log.d(TAGMap, "isServicesOK: checking google services version");
 
         int available = GoogleApiAvailability.getInstance().isGooglePlayServicesAvailable(MapsActivity.this);
 
         if (available == ConnectionResult.SUCCESS) {
             //everything is fine and the user can make map requests
-            Log.d(TAG, "isServicesOK: Google Play Services is working");
+            Log.d(TAGMap, "isServicesOK: Google Play Services is working");
             return true;
         } else if (GoogleApiAvailability.getInstance().isUserResolvableError(available)) {
             //an error occured but we can resolve it
-            Log.d(TAG, "isServicesOK: an error occured but we can fix it");
+            Log.d(TAGMap, "isServicesOK: an error occured but we can fix it");
             Dialog dialog = GoogleApiAvailability.getInstance().getErrorDialog(MapsActivity.this, available, ERROR_DIALOG_REQUEST);
             dialog.show();
         } else {
@@ -124,5 +139,37 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
         return false;
     }
 
+    ///// LOCALISATION DU SMARTPHONE /////
+    private void getLocalisationDevice (){
+        Log.d(TAGLocalisation, "getLocalisationDevice : Localisation actuelle de l'appareil");
+        mFusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(MapsActivity);
+
+        try {
+            if (permissionsAccordee){
+                final Task location = mFusedLocationProviderClient.getLastLocation();
+                location.addOnCompleteListener(new OnCompleteListener() {
+                    @Override
+                    public void onComplete(@NonNull Task task) {
+                        if (task.isSuccessful()){
+                            Log.d(TAGLocalisation, "onComplete : Localisation actuelle trouvé !");
+                            Location localisationCourante = (Location) task.getResult();
+                            bougerCamera(new LatLng(localisationCourante.getLatitude(),
+                                    localisationCourante.getLongitude()), DEFAULT_ZOOM);
+                        } else {
+                            Log.d(TAGLocalisation, "onComplete : Localisation actuelle null");
+                            Toast.makeText(MapsActivity, "impossible de récupérer la localisation actuelle", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+            }
+        } catch (SecurityException Se) {
+            Log.d(TAGLocalisation,"getDeviceLocation : SecurityException : " + Se.getMessage());
+        }
+    }
+
+    private void bougerCamera(LatLng latitudeLongitude, float zoom) {
+        Log.d(TAGLocalisation, "mouvementCamera : latitude : " + latitudeLongitude.latitude + ", longitude : " + latitudeLongitude.longitude);
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latitudeLongitude, zoom));
+    }
 
 }
