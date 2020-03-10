4e8d3d02dffc97064e784e8a5420e0603c397429
Shayne <khaldi.alternance@gmail.com>
2020-03-10 22:38:56 +0100

    feat-GP-0006 : position GPS actuel fonctionel
    TODO : verification wifi et gps activé par le user

--- app/src/main/java/sio/example/gestion_patrimoine/MapsActivity.java
+++ app/src/main/java/sio/example/gestion_patrimoine/MapsActivity.java
@@ -3,6 +3,7 @@ package sio.example.gestion_patrimoine;
 import android.Manifest;
 import android.app.Dialog;
 import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.Toast;
@@ -19,10 +20,11 @@ import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 
-public class MapsActivity extends AppCompatActivity implements OnMapReadyCallback {
+public class MapsActivity extends AppCompatActivity implements OnMapReadyCallback, GoogleMap.OnMyLocationClickListener, GoogleMap.OnMyLocationButtonClickListener {
 
     // CONSTANTES
-    private static final String TAG = "MapActivity";
+    private static final String TAGMap = "MapActivity";
+    private static final String TAGLocalisation = "Localisation";
     private static final int ERROR_DIALOG_REQUEST = 9001;
     private static final String FINE_LOCATION = Manifest.permission.ACCESS_FINE_LOCATION;
     private static final String COARSE_LOCATION = Manifest.permission.ACCESS_COARSE_LOCATION;
@@ -35,8 +37,14 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
     @Override
     public void onMapReady(GoogleMap googleMap) {
         Toast.makeText(this, "Map Prête", Toast.LENGTH_SHORT).show();
-        Log.d(TAG, "onMapReady: map correctement afficher");
+        Log.d(TAGMap, "onMapReady: map correctement afficher");
         mMap = googleMap;
+
+        if (permissionsAccordee) {
+            getLocalisationDevice();
+            mMap.setOnMyLocationButtonClickListener(this);
+            mMap.setOnMyLocationClickListener(this);
+        }
     }
 
     @Override
@@ -51,15 +59,20 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
 
     }
 
+    ///// INITIALISE LA MAP /////
     private void initMap() {
-        Log.d(TAG, "initMap: Map Initialisé");
+        Log.d(TAGMap, "initMap: Map Initialisé");
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
 
-        mapFragment.getMapAsync(MapsActivity.this);
+        // Eviter le NullPointerException
+        if (mapFragment != null) {
+            mapFragment.getMapAsync(MapsActivity.this);
+        }
     }
 
+    ///// RECUPERE LA PERMISSION DONNER PAR LE USER /////
     private void getPermissionLocalisation() {
-        Log.d(TAG, "getLocationPermission: Permissions de localisation");
+        Log.d(TAGMap, "getLocationPermission: Permissions de localisation");
         String[] permissions = {FINE_LOCATION, COARSE_LOCATION};
 
         if (ContextCompat.checkSelfPermission(this.getApplicationContext(),
@@ -80,42 +93,36 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
         }
     }
 
+    ///// RETOUR DE LA DEMANDE DE PERMISSION DU GPS /////
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        Log.d(TAG, "onRequestPermissionsResult: Appel de la méthode");
+        Log.d(TAGMap, "onRequestPermissionsResult: Appel de la méthode");
         permissionsAccordee = false;
 
-        switch (requestCode) {
-            case LOCATION_PERMISSION_REQUEST_CODE: {
-                if (grantResults.length > 0) {
-                    for (int grantResult : grantResults) {
-                        if (grantResult != PackageManager.PERMISSION_GRANTED) {
-                            permissionsAccordee = false;
-                            Log.d(TAG, "onRequestPermissionsResult: Permission refusé");
-                            return;
-                        }
-                    }
-                    Log.d(TAG, "onRequestPermissionsResult: Permission accordé");
-                    permissionsAccordee = true;
-                    // Initialisé la Map Google
-                    initMap();
-                }
+        if (requestCode == LOCATION_PERMISSION_REQUEST_CODE) {
+            if (permissions.length == 1 &&
+                    permissions[0] == Manifest.permission.ACCESS_FINE_LOCATION &&
+                    grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                mMap.setMyLocationEnabled(true);
+            } else {
+                // Permission was denied. Display an error message.
             }
         }
     }
 
+    ///// VERIFICATION DES SERVICES GOOGLE FONCTIONELS /////
     public boolean isServicesOK() {
-        Log.d(TAG, "isServicesOK: checking google services version");
+        Log.d(TAGMap, "isServicesOK: checking google services version");
 
         int available = GoogleApiAvailability.getInstance().isGooglePlayServicesAvailable(MapsActivity.this);
 
         if (available == ConnectionResult.SUCCESS) {
             //everything is fine and the user can make map requests
-            Log.d(TAG, "isServicesOK: Google Play Services is working");
+            Log.d(TAGMap, "isServicesOK: Google Play Services is working");
             return true;
         } else if (GoogleApiAvailability.getInstance().isUserResolvableError(available)) {
             //an error occured but we can resolve it
-            Log.d(TAG, "isServicesOK: an error occured but we can fix it");
+            Log.d(TAGMap, "isServicesOK: an error occured but we can fix it");
             Dialog dialog = GoogleApiAvailability.getInstance().getErrorDialog(MapsActivity.this, available, ERROR_DIALOG_REQUEST);
             dialog.show();
         } else {
@@ -124,5 +131,24 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
         return false;
     }
 
+    ///// LOCALISATION DU SMARTPHONE /////
+    private void getLocalisationDevice() {
+        if (PackageManager.PERMISSION_GRANTED == ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)) {
+            mMap.setMyLocationEnabled(true);
+        }
+    }
 
+    @Override
+    public void onMyLocationClick(@NonNull Location location) {
+        Toast.makeText(this, "Current location:\n" + location, Toast.LENGTH_LONG).show();
+
+    }
+
+    @Override
+    public boolean onMyLocationButtonClick() {
+        Toast.makeText(this, "MyLocation button clicked", Toast.LENGTH_SHORT).show();
+        // Return false so that we don't consume the event and the default behavior still occurs
+        // (the camera animates to the user's current position).
+        return false;
+    }
 }
